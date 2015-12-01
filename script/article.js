e23c1ac49081fd25f216571931adda633117efcc
Yueyue07"git config --global user.name Yueyue0 <Q_X@Q-Xs-MacBook-Pro.local>
2015-12-01 13:20:08 -0800

    Put last homework's code files here as starting point

---
+++ script/article.js
@@ -0,0 +1,35 @@
+function Article(props,i) {
+    this.title = props.title;
+    this.category = props.category;
+    this.author = props.author;
+    this.authorUrl = props.authorUrl;
+    this.publishedOn = props.publishedOn;
+    this.body = props.body;
+    this.i = i;
+}
+
+
+Article.prototype.toHTML = function () {
+    //Copy #template artible with clone
+    var $articleTemplate = $('#template').clone().attr('id',   'template' + '_' + this.i);
+    //insert title
+    $articleTemplate.find('.title').html(this.title);
+    // insert author
+    $articleTemplate.find('.author').html(this.author);
+
+    //past date
+    var pastdate = new Date(this.publishedOn);
+    var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+    //current date
+    var currentdate = new Date();
+    var diffDays = Math.round(Math.abs((currentdate - pastdate )/(oneDay)-1));
+    //insert post days
+    $articleTemplate.find('.postDays').html(diffDays);
+    //insert link to author
+    $articleTemplate.find('a.author').attr("href",this.authorUrl);
+    // insert body
+    $articleTemplate.find('.body').html(this.body);
+    //append to main tag
+    $articleTemplate.appendTo('main');
+
+}
