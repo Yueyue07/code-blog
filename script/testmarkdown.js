8cfbd21c08f2ebb9357e33efd9fccde1e345f3ca
Yueyue07 <xww.qyy@gmail.com>
2015-12-07 15:22:54 -0800

    create preview,mardownand secret URl,but code syntax and json data show didn't work yet

--- script/testmarkdown.js
+++ script/testmarkdown.js
@@ -1,10 +1,42 @@
 //select body box element, listen for user input, move to preview box
 // $('#body').on('change', ).html();
-$('textarea#body').on('change', function(event){
-    event.preventDefault();
-    var content = marked($(this).val());
-    $('#preview').html(content);
-  });
+var preview = function() {
+  $('input#title').on('change', function(event){
+      event.preventDefault();
+      var titlevalue = marked($(this).val());
+      $('#preview').append('title: ' + titlevalue);
+    });
+
+  $('input#name').on('change', function(event){
+      event.preventDefault();
+      var namevalue = marked($(this).val());
+      $('#preview').append('name: ' + namevalue);
+    });
+
+  $('input#authorUrl').on('change', function(event){
+      event.preventDefault();
+      var authorUrlvalue = marked($(this).val());
+      $('#preview').append('authorUrl' + authorUrlvalue);
+    });
+
+  $('textarea#body').on('change', function(event){
+      event.preventDefault();
+      var bodyvalue = marked($(this).val());
+      $('#preview').append('body: ' + bodyvalue);
+    });
+
+    // var JSONdata = $("form#article").serialize();
+    // console.log(JSONdata);
+  //
+  // $('#submitbutton').click(function(){
+  //     $('#jsondata').append(JSONdata);
+  // });
+
+}
+
+$(function(){
+    preview();
+});
 
 
 //
