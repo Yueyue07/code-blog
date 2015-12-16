4a90ac5cead7e7d438f8eadf809bceeb30efe5a9
Yueyue07 <xww.qyy@gmail.com>
2015-12-16 14:15:57 -0800

    change the links of github and twitter

--- scripts/repos.js
+++ scripts/repos.js
@@ -4,15 +4,15 @@ repos.all = [];
 
 repos.requestAll = function(callback){
   $.ajax({
+    url: '/github/users/Yueyue07/repos',
     type: 'GET',
-    url:'https://api.github.com/users/Yueyue07/repos?sort=pushed',
-    headers: { Authorization: 'token ' + githubToken} // '/github/users/yueyue07/repos'
-  }).done(function(data){
-    console.log(data[0]);
-    data.forEach(function(item){
-      item['pushed_at'] = item['pushed_at'].slice(0,10);
-    });
-    repos.all = data;
-    console.log(repos.all);
+    // url:'https://api.github.com/users/Yueyue07/repos?sort=pushed',
+    // headers: { Authorization: 'token ' + githubToken} // '/github/users/yueyue07/repos'
+    success: function(data,message,xhr) {
+      data.forEach(function(item){
+        item['pushed_at'] = item['pushed_at'].slice(0,10);
+      });
+      repos.all=data;
+    }
   }).done(callback);
 };
